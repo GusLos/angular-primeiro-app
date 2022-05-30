08b6c4d472ad51083a4cab60f0ef677d73494c44
GusLos <gu.losada@hotmail.com>
2022-05-30 14:33:30 -0300

    bootstrap não funcionando, resolver

---
+++ src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-angular-cli',
+  templateUrl: './ola-angular-cli.component.html',
+  styleUrls: ['./ola-angular-cli.component.css']
+})
+export class OlaAngularCliComponent /*implements OnInit*/ {
+
+  //usado para injeção de dependencias
+  // constructor() { }
+
+  //Usado para executar uma (função)/(bloco de código) assim q o componente "nasce"
+  //chamado depois do construtor
+  // ngOnInit(): void {
+  // }
+
+  
+
+}
---
+++ src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ola-angular',
+    // template: `
+    //     <p>Olá, angular.</p>
+    // `
+    templateUrl: './ola-angular.component.html',
+    styleUrls: ['./ola-angular.component.css']
+})
+export class OlaAngularComponent{}
